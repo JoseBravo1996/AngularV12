d0790bd33f35711324e4df2a320b0c80153f817d
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2022-01-15 23:34:05 -0300

    add configuration folders

--- angular-web/src/app/app.module.ts
+++ angular-web/src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from '@core/core.module';
+import { DataModule } from '@data/data.module';
+import { ModulesModule } from '@modules/modules.module';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    DataModule,
+    ModulesModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
